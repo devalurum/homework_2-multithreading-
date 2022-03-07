dc228dc4bf1b9f052f77d814e57db7ffac6cb28d
Alexander Urumov <sahaheh@mail.ru>
2022-03-07 21:18:56 +0800

    added not implemented code

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,11 +1,16 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -15,25 +20,54 @@ public class FileProcessor {
     private static final Logger logger = getLogger(FileProcessor.class);
     public static final int CHUNK_SIZE = 2 * getRuntime().availableProcessors();
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(CHUNK_SIZE);
+    private final ExecutorService executorServiceForWriter = Executors.newSingleThreadExecutor();
+
+    private final LineProcessor<Integer> lineCounter = new LineCounterProcessor();
+
+    private final Exchanger<Pair<String, Integer>> exchanger = new Exchanger<>();
+
+
     public void process(@Nonnull String processingFileName, @Nonnull String resultFileName) {
         checkFileExists(processingFileName);
 
         final File file = new File(processingFileName);
-        // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+
+        // запускаем FileWriter в отдельном потоке
+        FileWriter fileWriter = new FileWriter(exchanger, resultFileName);
+        executorServiceForWriter.execute(fileWriter);
 
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
+
             while (scanner.hasNext()) {
-                // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
+                // вычитываем CHUNK_SIZE строк для параллельной обработки
+                List<String> tempList = new ArrayList<>(CHUNK_SIZE);
 
-                // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
+                for (int i = 0; i < CHUNK_SIZE; i++) {
+                    if (scanner.hasNext()) {
+                        tempList.add(scanner.nextLine());
+                    }
+                }
+                // обрабатываем строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
+                List<Future<Pair<String, Integer>>> futureList = tempList
+                        .stream()
+                        .map(line -> executorService.submit(() -> lineCounter.process(line)))
+                        .collect(Collectors.toList());
 
-                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
+                // передаём обработанные данные в поток, записывающий данные в результирующий файл
+                for (Future<Pair<String, Integer>> pairFuture : futureList) {
+                    // future.get() блокирует выполнение до тех пор, пока задача не будет завершена
+                    exchanger.exchange(pairFuture.get());
+                }
             }
-        } catch (IOException exception) {
+
+        } catch (IOException | InterruptedException | ExecutionException exception) {
             logger.error("", exception);
         }
 
-        // TODO: NotImplemented: остановить поток writerThread
+        // остановливаем поток writer
+        executorService.shutdown();
+        executorServiceForWriter.shutdownNow();
 
         logger.info("Finish main thread {}", Thread.currentThread().getName());
     }
--- src/main/java/ru/digitalhabbits/homework2/FileWriter.java
+++ src/main/java/ru/digitalhabbits/homework2/FileWriter.java
@@ -1,18 +1,60 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Exchanger;
 
 import static java.lang.Thread.currentThread;
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class FileWriter
-        implements Runnable {
+public class FileWriter implements Runnable {
     private static final Logger logger = getLogger(FileWriter.class);
+
+    public static final String FORMAT = "%s %d\n";
+
+    private final Exchanger<Pair<String, Integer>> exchanger;
+    private final Path resultFile;
+
+    public FileWriter(Exchanger<Pair<String, Integer>> exchanger, @Nonnull String outputFile) {
+        this.exchanger = exchanger;
+        this.resultFile = createFile(outputFile);
+    }
+
+    private Path createFile(String outputFile) {
+        Path path = Path.of(outputFile);
+        try {
+            if (Files.notExists(path)) {
+                return Files.createFile(path);
+            } else return path;
+        } catch (IOException e) {
+            logger.error("", e);
+            return path;
+        }
+    }
 
     @Override
     public void run() {
         logger.info("Started writer thread {}", currentThread().getName());
-
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Pair<String, Integer> pair = exchanger.exchange(null);
+                writeLine(pair);
+            } catch (IOException | InterruptedException e) {
+                logger.error("", e);
+            }
+        }
         logger.info("Finish writer thread {}", currentThread().getName());
+    }
+
+    private void writeLine(Pair<String, Integer> line) throws IOException {
+        Files.writeString(resultFile,
+                String.format(FORMAT, line.getKey(), line.getValue()),
+                StandardOpenOption.APPEND);
     }
 }
